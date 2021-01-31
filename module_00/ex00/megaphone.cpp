f476275b37edfec212f7f162ff9fb4865f60e5a3
nalysann <urbilya@gmail.com>
2021-01-31 13:35:23 +0300

    Add module 00 ex00

---
+++ module_00/ex00/megaphone.cpp
@@ -0,0 +1,15 @@
+#include <cctype>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    } else {
+        for (int i = 1; i < argc; ++i) {
+            for (char* str = argv[i]; *str != '\0'; ++str) {
+                std::cout << static_cast<char>(std::toupper(*str));
+            }
+        }
+        std::cout << std::endl;
+    }
+}
