67a7d4e4bcfa3fbe6951f853e8c786ff30926fe0
nalysann <urbilya@gmail.com>
2021-02-02 14:54:20 +0300

    Finish module 01 ex10
    Can't handle errors properly using only C++ instruments though.

--- module_01/ex10/main.cpp
+++ module_01/ex10/main.cpp
@@ -1,3 +1,42 @@
-int main() {
+#include <fstream>
+#include <iostream>
+#include <string>
 
+void readFromConsole() {
+    std::string input;
+    while (getline(std::cin, input)) {
+        std::cout << input << std::endl;
+    }
+}
+
+void readFromFile(const std::string& filename) {
+    std::ifstream ifs(filename.c_str());
+
+    if (ifs.fail()) {
+        std::cout << "cato9tails: " << filename
+                  << ": Can't open file" << std::endl;
+        return;
+    }
+
+    std::string input;
+    while (getline(ifs, input)) {
+        std::cout << input << std::endl;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        readFromConsole();
+    } else {
+        for (int i = 1; i < argc; ++i) {
+            std::string filename(argv[i]);
+            if (filename == "-") {
+                std::cin.clear();
+                rewind(stdin);
+                readFromConsole();
+            } else {
+                readFromFile(filename);
+            }
+        }
+    }
 }
